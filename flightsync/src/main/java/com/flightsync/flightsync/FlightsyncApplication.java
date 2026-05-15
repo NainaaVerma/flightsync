6ee9531e61dd5f2108983a99de80fd363a56c6a6
Naina Verma <nainav8938@gmail.com>
2026-05-16 04:12:41 +0530

    initial project setup

---
+++ flightsync/src/main/java/com/flightsync/flightsync/FlightsyncApplication.java
@@ -0,0 +1,13 @@
+package com.flightsync.flightsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsyncApplication.class, args);
+	}
+
+}
---
+++ flightsync/src/test/java/com/flightsync/flightsync/FlightsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightsync.flightsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
